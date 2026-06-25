daa3e173e3b6015d12cbaecc0bb950ae95c7161a
gpcoding15 <gisela.pardini15@gmail.com>
2026-06-25 14:17:35 -0300

    valueerror added when properties are empty strings

--- planet.py
+++ planet.py
@@ -2,3 +2,15 @@ class Planet():
     def __init__(self, name, planet_type, star):
         if not isinstance(name, str):
             raise TypeError("name, planet type, and star must be strings")
+
+        if not isinstance(planet_type, str):
+            raise TypeError("name, planet type, and star must be strings")
+
+        if not isinstance(star, str):
+            raise TypeError("name, planet type, and star must be strings")
+
+        if not name:
+            raise ValueError("name, planet_type, and star must be non-empty strings")
+        
+        if not planet_type:
+            raise ValueError("name, planet_type, and star must be non-empty strings")
