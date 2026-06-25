e6b308966d032b67e6909ec6950b2bfc3370ac07
gpcoding15 <gisela.pardini15@gmail.com>
2026-06-25 14:19:42 -0300

    properties assigned

--- planet.py
+++ planet.py
@@ -1,5 +1,9 @@
 class Planet():
     def __init__(self, name, planet_type, star):
+        self.name = name
+        self.planet_type = planet_type
+        self.star = star
+
         if not isinstance(name, str):
             raise TypeError("name, planet type, and star must be strings")
 
@@ -13,4 +17,7 @@ class Planet():
             raise ValueError("name, planet_type, and star must be non-empty strings")
         
         if not planet_type:
+            raise ValueError("name, planet_type, and star must be non-empty strings")
+
+        if not star:
             raise ValueError("name, planet_type, and star must be non-empty strings")
